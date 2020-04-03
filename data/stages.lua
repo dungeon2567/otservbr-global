808fbb2715fd267fb59d457b8fdc3554f750ce0c
dungeon2567 <31074903+dungeon2567@users.noreply.github.com>
2020-04-03 07:02:01 -0300

    .

--- data/stages.lua
+++ data/stages.lua
@@ -4,22 +4,18 @@
 experienceStages = {
 	{
 		minlevel = 1,
-		maxlevel = 8,
-		multiplier = 7
-	}, {
-		minlevel = 9,
 		maxlevel = 20,
-		multiplier = 6
+		multiplier = 100
 	}, {
 		minlevel = 21,
 		maxlevel = 50,
-		multiplier = 5
+		multiplier = 60
 	}, {
 		minlevel = 51,
 		maxlevel = 100,
-		multiplier = 4
+		multiplier = 30
 	}, {
 		minlevel = 101,
-		multiplier = 2
-	}
+		multiplier = 10
+	},
 }
